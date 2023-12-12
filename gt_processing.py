9e8269e3663288ccd6ed1c6e055a8b4edb9368ab
DO HYUN JEON <59910448+jdobbang@users.noreply.github.com>
2023-12-12 16:26:55 +0900

    posetrack21 gt postprocess

---
+++ gt_processing.py
@@ -0,0 +1,30 @@
+import os
+from pdb import set_trace
+import joblib
+import tqdm
+import numpy as np
+
+path = '/home/dobbang/jeon/PoseTrack21/eval/posetrack21/posetrack21/data/gt/PoseTrackReID/posetrack_data/mot/val'
+videos = np.sort(np.array(os.listdir(path)))
+data_gt = joblib.load('/home/dobbang/jeon/phalp_original/_DATA/posetrack/posetrack18_gt_data.pickle') 
+
+for v in range(len(videos)):
+    video = videos[v]
+    data=data_gt[video]
+    
+    gt_txt = os.path.join(path,video,'gt/gt.txt')
+    gt_sampling = open(path+'/'+video+'/gt/gt_sampling.txt','w')
+    
+    for t,frames in enumerate(data.keys()):
+        idx = t+1
+        print(t,frames)
+        f = open(gt_txt)
+        lines = f.readlines()
+        for line in lines:
+            if int(str(line).split(',')[0]) == int(str(frames).split('.')[0])+1:
+                tmp_line = str(line).split(',')[1:]
+                tmp_line.insert(0,str(idx))
+                new_line = ','.join(tmp_line)
+                gt_sampling.write(new_line)
+    gt_sampling.close()
+                
